e28ad4e6e5127ac2c8992c315396a6f4630060d0
Rodrigo José Sousa Santana Rodrigo <rodrigosantana.dev@gmail.com>
2026-04-28 19:59:26 -0300

    Inicializando projeto FalaEscrita

---
+++ backend/src/main/java/com/falaescrita/backend/FalaEscritaApplication.java
@@ -0,0 +1,12 @@
+package com.falaescrita.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaEscritaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FalaEscritaApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/falaescrita/backend/FalaEscritaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.falaescrita.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalaEscritaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
